417bd2486e0d2e861307f0eda0172700368efc47
JAVIERarka09 <javier_pagsidan_33rpl@student.smktelkom-mlg.sch.id>
2024-09-25 15:09:35 +0700

    Add files via upload

---
+++ DVD.java
@@ -0,0 +1,48 @@
+public class DVD extends product {
+    private int length;
+    private String rating;
+    private String studio;
+
+    public DVD(int number, String name, int quantity, double price, String artist, int numSong, String label ) {
+        super(number,name,quantity,price);
+        this.length = length;
+        this.rating = rating;
+        this.studio = studio;
+    }
+    
+    public int getLength() {
+        return this.length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public String getRating() {
+        return this.rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+    public String getStudio() {
+        return this.studio;
+    }
+
+    public void setStudio(String studio) {
+        this.studio = studio;
+    }
+
+    //Override dari superclass Product
+    public void print() {
+        super.print();
+        System.out.println("Length\t\t:" + length);
+        System.out.println("Rating\t\t:" + rating);
+        System.out.println("Studio\t\t:" + studio);
+
+  
+	}
+}
+
+
